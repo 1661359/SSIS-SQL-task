7e9d74fada31c6ddf2a927fec4d83c9d15ef64ed
dimdim <1661359@mail.ru>
2016-05-20 13:18:51 +0300

    Added operations with temporary table

---
+++ SSIS-SQL-task/SSIS-SQL-task/SqlScripts/CreateTemporaryDestinationTable.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [Temporary Destination] (
+    [Product] nvarchar(255),
+    [Customer] nvarchar(255),
+    [TransactionId] float,
+    [Price] float,
+    [Region] nvarchar(255),
+    [Category] nvarchar(255),
+    [Date] datetime
+)
+GO
---
+++ SSIS-SQL-task/SSIS-SQL-task/SqlScripts/DropTemporaryTable.sql
@@ -0,0 +1,4 @@
+DROP TABLE [Temporary Destination]
+GO
+
+
