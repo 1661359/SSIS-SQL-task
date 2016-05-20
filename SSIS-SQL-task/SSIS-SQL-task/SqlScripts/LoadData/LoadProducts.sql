4769cb4db5b43821aeed2f2e6367c18f082dfac7
dimdim <1661359@mail.ru>
2016-05-20 16:17:16 +0300

    Product loading fixed

--- SSIS-SQL-task/SSIS-SQL-task/SqlScripts/LoadData/LoadProducts.sql
+++ SSIS-SQL-task/SSIS-SQL-task/SqlScripts/LoadData/LoadProducts.sql
@@ -1,9 +1,11 @@
 insert into [dbo].[Product] (Name, Price, CategoryID)
-select temporaryData.Product, temporaryData.Price, category.CategoryID
-from (select Product, max(td.Date) as LastDate
-			from [TemporaryData] as td
-			group by td.Product) as productWithLastDate
-		join [TemporaryData] as temporaryData
-		on temporaryData.Product = productWithLastDate.Product and temporaryData.Date = productWithLastDate.LastDate 
+select Product, Price, CategoryID
+from (	select productWithLastDate.Product, max(temporaryData.Price) as Price, max(temporaryData.Category) as Category
+		from(	select Product, max(Date) as LastDate
+				from [TemporaryData]  
+				group by [Product]) as productWithLastDate
+	    join [TemporaryData] as temporaryData
+		on productWithLastDate.Product = temporaryData.Product and temporaryData.Date = productWithLastDate.LastDate
+		group by productWithLastDate.Product) as productWithCategoryName
 join [Category] as category
-on category.Name = temporaryData.Category
+on category.Name = productWithCategoryName.Category
